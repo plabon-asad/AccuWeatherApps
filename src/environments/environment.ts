1bdf5593ad9cc9e59a6f5731bbe6535a6871b389
plabon-asad <plabondotasad@gmail.com>
2022-04-11 08:58:34 +0600

    ui update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     baseUrl: 'http://dataservice.accuweather.com/locations/v1',
     currentDataUrl: 'http://dataservice.accuweather.com/currentconditions/v1',
     forecastUrl: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day',
-    key: 'vXIoYA889XJsmrLOGL7GGvrfJsbOG9S7',
+    key: 'bsy8Xh98AA6wRDaJblGv4u4P4egGMGoT',
   },
 };
 
